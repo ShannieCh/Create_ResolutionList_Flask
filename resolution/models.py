c3b3a8728e6c0bbf640c59bdfdfbf3f39d831ae7
ShannieCh <e.sh.chekani@gmail.com>
2026-02-15 21:09:17 +0100

    Finalizing security fixes and documentation

--- resolution/models.py
+++ resolution/models.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
 	username = db.Column(db.String(30), unique=True, nullable=False)
 	email = db.Column(db.String(60), unique=True, nullable=False)
 	password = db.Column(db.String(60), nullable=False)
-	posts = db.relationship('Post', backref='author', lazy=True, cascade='all, delete-orphan')
+	posts = db.relationship('Post', backref='author', lazy=True)
 
 	def __repr__(self):
 		return f'{self.__class__.__name__}({self.id}, {self.username})'
